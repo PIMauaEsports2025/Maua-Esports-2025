be004dc52da0affad5593484a994db125d69da86
Let <leticiacarvalhosilva2006@gmail.com>
2025-05-05 10:13:38 -0300

    feat: Contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import '../styles/Contact.css';
-import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
-import mauaLogo from '../assets/ui/maua-branco.png';
-import { CiLogin } from "react-icons/ci";
 import Header from './Layout/Header';
 import Footer from './Layout/Footer';
 
@@ -12,13 +8,15 @@ const Contact = () => {
         <div className="contato-container">
             <Header />
 
-            <section className="contato-banner">
-                <h1>Contato</h1>
+            <main className="comeco-content">
+                <h1>CONTATO</h1>
                 <p>Preencha o formulário abaixo com sua mensagem, dúvida ou sugestão. 
                     As informações enviadas serão encaminhadas diretamente para o nosso 
                     e-mail institucional e entraremos em contato o mais breve possível.
                 </p>
-            </section>
+            </main>
+
+            
 
             <section className="contato-form">
                 
