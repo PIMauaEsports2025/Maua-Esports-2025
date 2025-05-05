3eb1a30a4caddc2d99e2300731878156f8b97ef1
Let <leticiacarvalhosilva2006@gmail.com>
2025-05-05 10:32:15 -0300

    feat(styles & contact): adiciona responsividade para seções de etapas e prêmios

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import '../styles/Contact.css';
+import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import mauaLogo from '../assets/ui/maua-branco.png';
+import { CiLogin } from "react-icons/ci";
 import Header from './Layout/Header';
 import Footer from './Layout/Footer';
 
-const Contact = () => {
+function Contact() {
+    const contact = []
     return (
         <div className="contato-container">
             <Header />
